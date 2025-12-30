fe5dd9a9efcf5dd296d9f75ccbb7be6bc2c19e4c
mikenorusis <mnorusis@gmail.com>
2025-12-30 12:47:49 -0500

    tnj 0.1.7
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/tnj.rb
+++ Formula/tnj.rb
@@ -1,11 +1,11 @@
 class Tnj < Formula
   desc "Your TUI application"  # ← Update with a real description
   homepage "https://github.com/mikenorusis/tnj"
-  version "0.1.2"  # ← Match your current/latest release version
+  version "0.1.7"  # ← Match your current/latest release version
 
   if Hardware::CPU.intel?
-    url "https://github.com/mikenorusis/tnj/releases/download/v#{version}/tnj-macos-x86_64"
-    sha256 "81048e4bc4f43097afd9b3e485085e7bb79f546cbaf1d9bf056475004b805e75"
+    url "https://github.com/mikenorusis/tnj/archive/refs/tags/v0.1.7.tar.gz"
+    sha256 "d848034d6f86916d48179d54d24d6ad18770176de73a80c4c85a77b410e6574f"
   else
     url "https://github.com/mikenorusis/tnj/releases/download/v#{version}/tnj-macos-aarch64"
     sha256 "05a9a186f09a23486966bd524618a2b752f96fc0fb66b68daaab0766a704f394"
