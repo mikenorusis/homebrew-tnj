fca19449654ce1be7f13bc87e79f7613e8c90bf2
mikenorusis <mnorusis@gmail.com>
2026-01-11 09:11:59 -0500

    tnj 0.1.16
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/tnj.rb
+++ Formula/tnj.rb
@@ -1,10 +1,10 @@
 class Tnj < Formula
   desc "Tasks, Notes, Journal - A lightweight terminal-based application"
   homepage "https://github.com/mikenorusis/tnj"
-  version "0.1.15"  # Start with your current version
+  version "0.1.16"  # Start with your current version
 
-  url "https://github.com/mikenorusis/tnj/releases/download/v0.1.15/tnj-macos-universal"
-  sha256 "09f3668febb11f75041f95c9d18f0ba9549c4b2f2343186272f7d78a1228345d"
+  url "https://github.com/mikenorusis/tnj/releases/download/v0.1.16/tnj-macos-universal"
+  sha256 "62af48bb5b80b1ec2acd13f33db094ca6040c69fe7d717ba5665fb653b2cc1f4"
 
   def install
     bin.install "tnj-macos-universal" => "tnj"
